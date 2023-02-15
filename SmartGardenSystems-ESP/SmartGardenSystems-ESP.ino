2aa4b491cba4ab755e41f8d650892af4bfaa013e
Danijel Strbad <87903457+DanijelStrbad@users.noreply.github.com>
2023-02-15 13:01:11 +0100

    WiFi operation mode - v0.5

--- SmartGardenSystems-ESP/SmartGardenSystems-ESP.ino
+++ SmartGardenSystems-ESP/SmartGardenSystems-ESP.ino
@@ -5,9 +5,9 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#define LED 0
-#define INIT_ON 8
-#define INIT_OFF 22
+#define LED 2
+#define INIT_ON 9
+#define INIT_OFF 17
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 60000);
@@ -15,6 +15,7 @@ ESP8266WebServer server(80);
 
 int day, H, minutes, seconds;
 int userLedOn = INIT_ON, userLedOff = INIT_OFF;
+int sec_counter = 0;
 
 
 void handleRoot() {
@@ -30,6 +31,10 @@ void handleRoot() {
     userLedOn = server.arg(0).toInt();
     userLedOff = server.arg(1).toInt();
     root_ret += "<h1>ON and OFF times set!</h1>";
+
+    Serial.print("ON and OFF time: ");
+    Serial.print(userLedOn);
+    Serial.println(userLedOff);
   }
   root_ret += "</body></html>";
 
@@ -52,22 +57,30 @@ void handleNotFound() {
 }
 
 
+void connect_to_wifi() {
+  WiFiManager wm;
+  bool res;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    res = wm.autoConnect("SmartGarden", "");
+
+    if (!res) {
+      Serial.println("Can't connect to Wi-Fi");
+    } else {
+      Serial.println("Connected to Wi-Fi!)");
+    }
+    delay(2500);
+  }
+  return;
+}
+
+
 void setup() {
   pinMode(LED, OUTPUT);
   WiFi.mode(WIFI_STA);
   Serial.begin(115200);
-  WiFiManager wm;
-  bool res;
 
-  res = wm.autoConnect("SmartGarden");
-
-  if (!res) {
-    Serial.println("Can't connect to Wi-Fi");
-  }
-  else {
-    Serial.println("Connected to Wi-Fi!)");
-  }
-
+  connect_to_wifi();
 
   timeClient.begin();
 
@@ -83,27 +96,34 @@ void setup() {
 }
 
 void loop() {
-  timeClient.update();
+  connect_to_wifi();
+  
+  if (sec_counter > 10) {
+    sec_counter = 0;
+    timeClient.update();
 
-  Serial.println(timeClient.getFormattedTime());
-  H = (timeClient.getHours());
-  seconds = (timeClient.getSeconds());
-  minutes = (timeClient.getMinutes());
-  day = (timeClient.getDay());
+    Serial.println(timeClient.getFormattedTime());
+    H = (timeClient.getHours());
+    seconds = (timeClient.getSeconds());
+    minutes = (timeClient.getMinutes());
+    day = (timeClient.getDay());
 
-  if (H <= userLedOff && H >= userLedOn) {
-    digitalWrite(LED, 1);
-    Serial.println("Lights ON");
-    Serial.print("H: ");
-    Serial.println(H);
-  }
-  else {
-    digitalWrite(LED, 0);
-    Serial.println("Lights OFF");
-    Serial.print("H: ");
-    Serial.println(H);
+    if (H <= userLedOff && H >= userLedOn) {
+      digitalWrite(LED, 1);
+      Serial.println("Lights ON");
+      Serial.print("H: ");
+      Serial.println(H);
+    }
+    else {
+      digitalWrite(LED, 0);
+      Serial.println("Lights OFF");
+      Serial.print("H: ");
+      Serial.println(H);
+    }
   }
 
   server.handleClient();
+
+  sec_counter++;
   delay(1000);
 }
